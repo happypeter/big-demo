cc8cd803669e8bc5f3829f1446483c7450acb4ab
Peter Wang <happypeter1983@gmail.com>
2016-08-06 11:43:05 +0800

    home img

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,9 +2,36 @@ import React, { PropTypes } from 'react';
 
 class Home extends React.Component {
   render () {
+    let styles={
+      root:{
+        width:'100%',
+        height:'100vh',
+        background: 'url("https://raw.githubusercontent.com/happypeter/big-demo/gh-pages/img/newming24.jpg") no-repeat fixed center center/cover'
+      },
+      title:{
+        color:'#fff',
+        textAlign:'center',
+        paddingTop:'40vh',
+        fontSize:'48px'
+      },
+      span:{
+        color:'#00BCD4',
+        fontSize:'36px'
+      },
+      cover:{
+        width:'100%',
+        height:'100%',
+        backgroundColor:'rgba(0,0,0,0.7)'
+      }
+    }
     return(
       <div>
-        Home
+        <div style={styles.root}>
+          <div style={styles.cover}>
+            <h1 style={styles.title}>欢迎光临<br/><span style={styles.span}>PETER CORNER</span></h1>
+          </div>
+        </div>
+        <h1 style={{color:'#777',textAlign:'center',lineHeight:'90px'}}>我的个人作品</h1>
       </div>
     )
   }
