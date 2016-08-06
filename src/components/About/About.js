cfd35792f6b43f5e51b2d9d058b80f8b5c5cb293
Peter Wang <happypeter1983@gmail.com>
2016-08-06 11:18:38 +0800

    move GithubAccount

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,12 @@
 import React, { PropTypes } from 'react';
+import GithubAccount from './GithubAccount/GithubAccount';
 
 class About extends React.Component {
   render () {
     return(
       <div>
-        About
+        <h1>About</h1>
+        <GithubAccount />
       </div>
     )
   }
