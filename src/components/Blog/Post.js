3f62e96e1997326410f5141d3aaaa95fff33a663
Peter Wang <happypeter1983@gmail.com>
2016-08-07 11:11:58 +0800

    get markdown raw

--- src/components/Blog/Post.js
+++ src/components/Blog/Post.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 
 class Post extends Component {
   constructor(){
     super();
     this.state={
+      rawContent: ''
     }
   }
+
+  componentDidMount(){
+    // use math random to avoid browser cache
+    let address = `https://raw.githubusercontent.com/happypeter/big-demo/master/posts/git-tips.md?v=${Math.random()}`
+    axios.get(address).then((res) => {
+      console.log(res);
+      console.log(address);
+      this.setState({
+        rawContent: res.data
+      });
+    });
+  }
+
   render(){
     return(
       <div>
         { this.props.params.title }
+        { this.state.rawContent }
       </div>
     )
   }
