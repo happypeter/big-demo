bb6ab1576a58a3c6071ce593158ff10cd5a16d69
Peter Wang <happypeter1983@gmail.com>
2016-08-09 11:55:39 +0800

    add highlight

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import {Router,hashHistory} from 'react-router';
 
 import routes from './routes.js';
 import './style/main.css';
+import './style/hljs.css';
 
 
 render(<Router routes={routes} history={hashHistory} />
